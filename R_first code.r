624f3d65f57f147bf9ad7537db04e9921d3e5933
Uyen Bui <72016762+Doriblue@users.noreply.github.com>
2021-10-22 14:20:06 +0200

    Update R_first code.r

--- R_first code.r
+++ R_first code.r
@@ -1,9 +1,20 @@
 #here are the input data
 # Costanza stream
 # cai nay cung goi la "Array"
-stream = c(100, 200, 300, 400, 500)
+water <- c(100, 200, 300, 400, 500)
 #Fish genomes also on the sream
 fishes <- c(10, 50, 60, 100, 200)
+
 #data frame, table
-data.frame (stream, fishes)
-View (data.frame (stream, fishes))
+streams <- data.frame (water, fishes)
+streams
+
+View (data.frame (water, fishes))
+
+#import and export data
+  #importing wd "working directory" 
+  setwd("/Users/buithuuyen/Desktop/lab/")
+  #export 
+            write.table (streams, file="my_monitoring.txt")
+#import a table sent by a colleage 
+read.table("my_monitoring.txt")
